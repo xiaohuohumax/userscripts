3a26ad925d1678b8bd31ce7ef0daa224f57ad8f0
xiaohuohumax <59244940+xiaohuohumax@users.noreply.github.com>
2025-03-03 12:04:39 +0800

    fix: :bug: 修改脚本名称和描述

--- apps/github/commit-sort/vite.config.ts
+++ apps/github/commit-sort/vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
     monkey({
       entry: 'src/index.ts',
       userscript: {
-        name: 'GitHub Commit Sort',
-        description: 'GitHub 仓库文件按提交时间排序',
+        name: 'GitHub 仓库文件按提交时间排序📅（GitHub Commit Sort）',
+        description: 'GitHub 仓库无法快速查看最新的变更文件？试试这个 GitHub 仓库文件按提交时间排序的用户脚本吧！',
         icon: 'https://github.githubassets.com/favicons/favicon-dark.png',
         namespace: 'xiaohuohumax/userscripts/github-commit-sort',
         license: 'MIT',
