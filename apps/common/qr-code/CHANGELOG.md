88834e9138662db92bcdcc68802297ef58429905
xiaohuohumax <xiaohuohumax@gmail.com>
2025-10-04 23:53:05 +0800

    feat: :sparkles: 解析二维码改为文本展示和复制按钮

--- apps/common/qr-code/CHANGELOG.md
+++ apps/common/qr-code/CHANGELOG.md
@@ -1,5 +1,11 @@
 # common-qr-code
 
+## 1.1.0
+
+### Minor Changes
+
+- 解析二维码改为文本展示和复制按钮
+
 ## 1.0.1
 
 ### Patch Changes
--- apps/common/qr-code/src/index.ts
+++ apps/common/qr-code/src/index.ts
@@ -1,7 +1,7 @@
 import { GM_registerMenuCommand, GM_setClipboard } from '$'
 import { Notify } from 'notiflix/build/notiflix-notify-aio'
+import swal from 'sweetalert'
 import { ID, VERSION } from 'virtual:meta'
-
 import { decodeQrCode } from './utils'
 
 console.log(`${ID}(v${VERSION})`)
@@ -16,8 +16,34 @@ GM_registerMenuCommand('Decode QR Code', () => {
     if (data === undefined) {
       return Notify.warning('未识别到二维码, 请确认图片是否有效')
     }
-    Notify.success('识别成功, 已复制到剪贴板')
-    GM_setClipboard(data, 'text')
+
+    const element = document.createElement('div')
+    element.textContent = data
+    element.style.fontSize = '16px'
+    element.style.padding = '10px'
+    element.style.border = '1px solid #ccc'
+    element.style.borderRadius = '5px'
+    element.style.maxHeight = '200px'
+    element.style.overflowY = 'auto'
+
+    swal({
+      icon: 'success',
+      title: '识别成功',
+      content: {
+        element,
+      },
+      buttons: {
+        confirm: {
+          text: '复制到剪贴板',
+          value: 'copy',
+        },
+      },
+    }).then((result) => {
+      if (result === 'copy') {
+        GM_setClipboard(data, 'text')
+        Notify.success('已复制到剪贴板')
+      }
+    })
   }).catch((error) => {
     Notify.failure('识别失败, 请检查图片是否有效')
     console.error(error)
