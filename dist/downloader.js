9b6eb14e357c1eb4bda88e5e5de35f4a8e9b03b5
xiaohuohumax <59244940+xiaohuohumax@users.noreply.github.com>
2025-12-15 12:47:33 +0000

    build: :package: build user script

--- dist/downloader.js
+++ dist/downloader.js
@@ -3,7 +3,7 @@
 // @namespace    xiaohuohumax/userscripts/downloader
 // @version      1.0.1
 // @author       xiaohuohumax
-// @description  资源下载器（下载资源、Zip 压缩、下载到本地）
+// @description  Downloader -- 资源下载器（下载资源、Zip 压缩、下载到本地）
 // @license      MIT
 // @icon         https://raw.githubusercontent.com/xiaohuohumax/logo/refs/heads/main/logos/logo.svg
 // @source       https://github.com/xiaohuohumax/userscripts.git
