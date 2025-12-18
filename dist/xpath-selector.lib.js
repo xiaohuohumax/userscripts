7f999a0d52a6ac3e76524e7ba86cfedb4bbc3347
xiaohuohumax <59244940+xiaohuohumax@users.noreply.github.com>
2025-12-18 14:43:25 +0000

    build: :package: build user script

--- dist/xpath-selector.lib.js
+++ dist/xpath-selector.lib.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        XPath Selector
 // @namespace   xiaohuohumax/userscripts/xpath-selector
-// @version     1.0.6
+// @version     1.1.0
 // @author      xiaohuohumax
 // @description XPath Selector -- 一个 XPath 选择器库，快速获取节点数据
 // @license     MIT
